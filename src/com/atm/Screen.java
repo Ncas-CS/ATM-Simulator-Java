eb90b300597f4cac6c2c3d050225933835ca5b5b
Muhammed Shebin VA <77822154+muhammedshebinva@users.noreply.github.com>
2021-04-19 14:39:29 +0530

    Frame Created

--- src/com/atm/Screen.java
+++ src/com/atm/Screen.java
@@ -1,4 +1,19 @@
 package com.atm;
 
 public class Screen {
+  JFrame jf;
+	
+	public Screen() {
+		jf=new JFrame("ATM");
+		jf.setSize(1000,1000);
+		jf.setLayout(null);
+		jf.setVisible(true);
+		jf.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	}
+		
+
+	public static void main(String[] args) {
+		
+		new Screen();
+	}
 }
