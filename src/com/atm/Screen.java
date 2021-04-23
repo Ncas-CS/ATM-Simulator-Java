82538a0797a4bd19592f677d668cc3b4646fc19b
Muhammed Shebin VA <77822154+muhammedshebinva@users.noreply.github.com>
2021-04-23 11:11:14 +0530

    First Screen Created

--- src/com/atm/Screen.java
+++ src/com/atm/Screen.java
@@ -1,153 +1,216 @@
-import java.awt.Color;
-import java.awt.Font;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
-
-import javax.swing.BorderFactory;
-import javax.swing.JButton;
-import javax.swing.JFrame;
-import javax.swing.JLabel;
-import javax.swing.JPanel;
-import javax.swing.SwingConstants;
-
-public class Screen implements ActionListener{
-	
-	JFrame jf;
-	JPanel panel;
-	JLabel label,displayLabel;
-	JButton sevenbtn,eightbtn,ninebtn,fourbtn,fivebtn,sixbtn,onebtn,twobtn,threebtn,zerobtn;
-	JButton enterbtn,clearbtn,endbtn;
-	
-	boolean isenterclicked;
-	String pinNumber;
-	
-	
-	public Screen() {
-		
-	    //HEADER
-		label=new JLabel();
-		label.setHorizontalAlignment(JLabel.CENTER);
-		label.setVerticalAlignment(JLabel.TOP);
-		label.setText("ENTER PIN");
-		label.setFont(new Font("Font. BOLD",Font.PLAIN,40));
-		label.setBounds(0,0,1000,100);
-		label.setOpaque(true);
-		
-		
-		//PIN DISPLAY
-		displayLabel=new JLabel();
-		displayLabel.setBounds(50, 100,260,50);
-		displayLabel.setBackground(Color.white);
-		displayLabel.setFont(new Font("Arial", Font.PLAIN, 40));
-		displayLabel.setBorder(BorderFactory.createLineBorder(Color.black, 5));
-		displayLabel.setHorizontalAlignment(SwingConstants.RIGHT);
-		
-		displayLabel.setOpaque(true);
-		
-		displayLabel.setText("****");
-		//BUTTONS
-		sevenbtn=new JButton("7");
-		sevenbtn.setBounds(50, 200, 60, 50);
-		sevenbtn.setFont(new Font("Arial", Font.PLAIN, 40));
-		eightbtn=new JButton("8");
-		eightbtn.setBounds(150, 200, 60, 50);
-		eightbtn.setFont(new Font("Arial", Font.PLAIN, 40));
-		ninebtn=new JButton("9");
-		ninebtn.setBounds(250, 200, 60, 50);
-		ninebtn.setFont(new Font("Arial", Font.PLAIN, 40));
-		
-		fourbtn=new JButton("4");
-		fourbtn.setBounds(50, 280, 60, 50);
-		fourbtn.setFont(new Font("Arial", Font.PLAIN, 40));
-		fivebtn=new JButton("5");
-		fivebtn.setBounds(150, 280, 60, 50);
-		fivebtn.setFont(new Font("Arial", Font.PLAIN, 40));
-		sixbtn=new JButton("6");
-		sixbtn.setBounds(250, 280, 60, 50);
-		sixbtn.setFont(new Font("Arial", Font.PLAIN, 40));
-		
-		onebtn=new JButton("1");
-		onebtn.setBounds(50, 360, 60, 50);
-		onebtn.setFont(new Font("Arial", Font.PLAIN, 40));
-		twobtn=new JButton("2");
-		twobtn.setBounds(150, 360, 60, 50);
-		twobtn.setFont(new Font("Arial", Font.PLAIN, 40));
-		threebtn=new JButton("3");
-		threebtn.setBounds(250, 360, 60, 50);
-		threebtn.setFont(new Font("Arial", Font.PLAIN, 40));
-		
-		zerobtn=new JButton("0");
-		zerobtn.setBounds(150, 440, 60, 50);
-		zerobtn.setFont(new Font("Arial", Font.PLAIN, 40));
-		
-		enterbtn=new JButton("Enter");
-		enterbtn.setBounds(440, 200, 190, 50);
-		enterbtn.setFont(new Font("Arial", Font.PLAIN, 30));
-		enterbtn.setBackground(Color.blue);
-		enterbtn.setForeground(Color.white);
-		enterbtn.addActionListener(this);
-		
-		clearbtn=new JButton("Clear");
-		clearbtn.setBounds(440, 280, 190, 50);
-		clearbtn.setFont(new Font("Arial", Font.PLAIN, 30));
-		clearbtn.setBackground(Color.green);
-		clearbtn.setForeground(Color.white);
-		
-		endbtn=new JButton("End");
-		endbtn.setBounds(440, 360, 190, 50);
-		endbtn.setFont(new Font("Arial", Font.PLAIN, 30));
-		endbtn.setBackground(Color.RED);
-		endbtn.setForeground(Color.white);
-		
-		
-		jf=new JFrame("ATM");
-		jf.setSize(1000,800);
-		jf.setLayout(null);
-		jf.setVisible(true);
-		jf.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		
-		jf.add(label);
-		jf.add(displayLabel);
-		jf.add(sevenbtn);
-		jf.add(eightbtn);
-		jf.add(ninebtn);
-		jf.add(fourbtn);
-		jf.add(fivebtn);
-		jf.add(sixbtn);
-		jf.add(onebtn);
-		jf.add(twobtn);
-		jf.add(threebtn);	
-		jf.add(zerobtn);
-		
-		jf.add(enterbtn);
-		jf.add(clearbtn);
-		jf.add(endbtn);
-		
-	}
-		
-
-	public static void main(String[] args) {
-		
-		new Screen();
-	}
 
 
-	
-	public void actionPerformed(ActionEvent e) {
-		
-		
-		
-		if(e.getSource()==enterbtn) {
-			pinNumber=displayLabel.getText();
-			
-			if(pinNumber.equals("****")) {
-				SecondScreen secondscreen=new SecondScreen();
-				secondscreen.setVisible(true);
+	import java.awt.*;
+        import java.awt.event.ActionEvent;
+	import java.awt.event.ActionListener;
+	import javax.swing.*;
+	    
+	   public class Screen implements ActionListener{
+		   
+		   JFrame jf;
+		   JPanel firstpanel,panel1,panel2,btnpanel,panel3,enterpanel;
+		   JLabel head,display;
+		   JButton x1,x2,x3,x4,x5,x6,x7,x8,x9,x10,x11,x12;
+		   JButton a1,a2,a3;
+		   String pinNumber;
+		   
+		   JPanel secondpanel;
+		   
+		   public Screen() {
+			   
+			 	jf = new JFrame("A T M");
+			 	 
+			 	//FIRST SCREEN
+			 	
+			   	firstpanel =new JPanel();
+			    firstpanel.setLayout(new BorderLayout());	
+			   	 
+			    //HEADING,DISPLAY
+			    
+			    panel1 = new JPanel();
+			     
+			    panel1.setLayout( new BorderLayout() );
+			    panel1.setBackground(Color.white);
+			    panel1.setBounds(0,0,400,150);
+			    	 
+			  	head=new JLabel();
+			    head.setText("ENTER PIN");
+			    head.setBounds(50, 100,260,50);
+			 	head.setBackground(Color.gray);
+			 	head.setFont(new Font("Arial", Font.BOLD, 30));
+			 	
+
+			   	display=new JLabel();
+			   	display.setText("****");
+				display.setBackground(Color.white);
+				display.setFont(new Font("Arial", Font.BOLD, 40));
+				display.setBorder(BorderFactory.createLineBorder(Color.black, 5));
+				 
+				panel1.add(head, "North");
+			   	panel1.add(display, "Center");
+			   	firstpanel.add( panel1, "North"); 
+			   	
+			   	//NUMBER BUTTONS
+			    
+			   	panel2 = new JPanel();
+			  	panel2.setLayout( new BorderLayout() );
+			  	 
+			   	btnpanel = new JPanel();
+			   	btnpanel.setLayout( new GridLayout(4, 3) );
+			   	
+			   	x1  = new JButton("7");
+			   	x1.setFont(new Font("Arial", Font.BOLD, 40));
+			   	x1.setBackground(Color.black);
+			   	x1.setForeground(Color.white);
+			   	
+			    x2  = new JButton("8");
+			  	x2.setFont(new Font("Arial", Font.BOLD, 40));
+			  	x2.setBackground(Color.black);
+			   	x2.setForeground(Color.white);
+			  
+			  	 
+			  	x3  = new JButton("9");
+			  	x3.setFont(new Font("Arial", Font.BOLD, 40));
+			  	x3.setBackground(Color.black);
+			   	x3.setForeground(Color.white);
+			  
+			  	 
+			  	x4  = new JButton("4");
+			  	x4.setFont(new Font("Arial", Font.BOLD, 40));
+			  	x4.setBackground(Color.black);
+			   	x4.setForeground(Color.white);
+			   	
+				x5  = new JButton("5");
+			  	x5.setFont(new Font("Arial", Font.BOLD, 40));
+			  	x5.setBackground(Color.black);
+			   	x5.setForeground(Color.white);
+			  
+			  	 
+			  	x6  = new JButton("6");
+			  	x6.setFont(new Font("Arial", Font.BOLD, 40));
+			  	x6.setBackground(Color.black);
+			   	x6.setForeground(Color.white);
+			  
+			  	 
+			  	x7  = new JButton("1");
+			  	x7.setFont(new Font("Arial", Font.BOLD, 40));
+			  	x7.setBackground(Color.black);
+			   	x7.setForeground(Color.white);
+			  
+			  	 
+			  	x8  = new JButton("2");
+			  	x8.setFont(new Font("Arial", Font.BOLD, 40));
+			  	x8.setBackground(Color.black);
+			   	x8.setForeground(Color.white);
+			   	
+			    
+			  	x9  = new JButton("3");
+			  	x9.setFont(new Font("Arial", Font.BOLD, 40));
+			  	x9.setBackground(Color.black);
+			   	x9.setForeground(Color.white);
+			  
+			  	 
+			  	x10 = new JButton("");
+			  	x10.setBackground(Color.white);
+			  	
+			  	x11 = new JButton("0");
+			  	x11.setFont(new Font("Arial", Font.BOLD, 40));
+			  	x11.setBackground(Color.black);
+			   	x11.setForeground(Color.white);
+			  
+			  	 
+			  	x12 = new JButton("");
+			  	x12.setBackground(Color.white);
+			  	
+				btnpanel.add(x1);
+			  	btnpanel.add(x2);
+			  	btnpanel.add(x3);
+			  	btnpanel.add(x4);
+			  	btnpanel.add(x5);
+			  	btnpanel.add(x6);
+			  	btnpanel.add(x7);
+			  	btnpanel.add(x8);
+			  	btnpanel.add(x9);
+			  	btnpanel.add(x10);
+			  	btnpanel.add(x11);
+			  	btnpanel.add(x12);
+			  	
+				panel2.add(btnpanel); 
+			  	firstpanel.add(panel2, "Center"); 
+			  	
+			  	//ENTER, CLEAR, EXIT BUTTONS
+			  	
+			  	panel3 = new JPanel();
+				panel3.setLayout( new BorderLayout() );
 				
-			}
+			  	enterpanel = new JPanel();
+			   	enterpanel.setLayout( new GridLayout(4, 1) );
+			   	 
+			   	a1  = new JButton("ENTER");
+			   	a1.setBackground(Color.blue);
+			   	a1.setForeground(Color.white);
+			   	a1.setFont(new Font("Arial", Font.BOLD, 30));
+			   	a1.addActionListener(this);
+			   	
+				
+			 	a2  = new JButton("CLEAR");
+			 	a2.setBackground(Color.green);
+			   	a2.setForeground(Color.white);
+			   	a2.setFont(new Font("Arial", Font.BOLD, 30));
+			 	
+			 	a3  = new JButton("EXIT");
+			 	a3.setBackground(Color.red);
+			   	a3.setForeground(Color.white);
+			   	a3.setFont(new Font("Arial", Font.BOLD, 30));
+
+			   	enterpanel.add(a1);
+			   	enterpanel.add(a2);
+			   	enterpanel.add(a3);
+			   	
+			   	enterpanel.setBackground(Color.white);
+			    
+			    panel3.add(enterpanel); 
+			    firstpanel.add(panel3, "East"); 
+			  	
+			 	jf.getContentPane().add(firstpanel, "Center");
+			    
+			   	 jf.setSize(400, 500);
+			   	 jf.setVisible(true);
+			     jf.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+			  
+			  //SECOND SCREEN
+			  
+			   	secondpanel =new JPanel();
+			   	secondpanel.setLayout(new BorderLayout());
+			   	secondpanel.setBackground(Color.green);
+			   	
+			   	jf.getContentPane().add(secondpanel,"Center");
+			 	
 			
 		}
+		   
+	      public static void main(String[] args){
+	    	  
+	  
+	    	  new Atm();
+	     
+	      }
+
 		
-	}
-	
-}
+		public void actionPerformed(ActionEvent e) {
+			
+
+			if(e.getSource()==a1) {
+				pinNumber=display.getText();
+				
+				if(pinNumber.equals("****")) {
+					 jf.remove(firstpanel);
+					 jf.add(secondpanel);
+		             jf.revalidate(); 
+		             jf.repaint();
+				}
+			}
+		}
+	   }
+
+
